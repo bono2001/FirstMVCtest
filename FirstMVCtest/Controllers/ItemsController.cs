92804671700b0930cc2cbf9f9b7ea0beb8f2159b
Bono Cichowski <2206279cichowski@zuyd.nl>
2024-09-22 20:39:01 +0200

    Add project files.

---
+++ FirstMVCtest/Controllers/ItemsController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FirstMVCtest.Data;
+using FirstMVCtest.Models;
+
+namespace FirstMVCtest.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ItemDb _context;
+
+        public ItemsController(ItemDb context)
+        {
+            _context = context;
+        }
+
+        // GET: Items
+
+        public async Task<IActionResult> Index(string sortOrder)
+        {
+            // Zorg ervoor dat de huidige sorteeroptie wordt opgeslagen in ViewData
+            ViewData["CurrentSort"] = sortOrder ?? "name_asc"; // Standaard naar "name_asc" als er geen sorteeroptie is
+
+            // Je sorteerlogica...
+            var items = from i in _context.Items.Include(i => i.Category)
+                        select i;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    items = items.OrderByDescending(i => i.Name);
+                    break;
+                case "price_asc":
+                    items = items.OrderBy(i => i.Price);
+                    break;
+                case "price_desc":
+                    items = items.OrderByDescending(i => i.Price);
+                    break;
+                case "date_asc":
+                    items = items.OrderBy(i => i.PurchaseDate);
+                    break;
+                case "date_desc":
+                    items = items.OrderByDescending(i => i.PurchaseDate);
+                    break;
+                case "category_asc":
+                    items = items.OrderBy(i => i.Category.Name);
+                    break;
+                case "category_desc":
+                    items = items.OrderByDescending(i => i.Category.Name);
+                    break;
+                default:
+                    items = items.OrderBy(i => i.Name); // Standaard sorteren op naam (A-Z)
+                    break;
+            }
+
+            return View(await items.ToListAsync());
+        }
+
+
+
+        // GET: Items/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Items/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Quantity,Price,Value,PurchaseDate,Condition,Origin,ForSale,ForTrade,CategoryId")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", item.CategoryId);
+            return View(item);
+        }
+
+        // GET: Items/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", item.CategoryId);
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Quantity,Price,Value,PurchaseDate,Condition,Origin,ForSale,ForTrade,CategoryId")] Item item)
+        {
+            if (id != item.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", item.CategoryId);
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items
+                .Include(i => i.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.Items.Any(e => e.Id == id);
+        }
+    }
+}
